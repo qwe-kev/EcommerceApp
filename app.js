402e2357793d093c68ebeec8374e8bbd15fe1394
qwe-kev <119116884+qwe-kev@users.noreply.github.com>
2023-01-10 12:17:08 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const adminRoutes = require('./Routes/admin');
+const shopRoutes = require('./Routes/shop');
+
+app.use(bodyParser.urlencoded({extended : false}));
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+})
+
+app.listen(4000, () => {
+    console.log('listening on port 4000');
+});
+
+
