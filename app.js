252762f94961eadadd93bcdf8a45c9363b748ccb
qwe-kev <kevray39@gmail.com>
2023-01-18 18:28:52 +0530

    added MySQL db CRUD operations

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const path = require('path');
 
 const express = require('express');
+
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+
+const db = require('./util/database');
 
 const app = express();
 
