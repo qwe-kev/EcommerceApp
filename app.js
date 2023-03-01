572fc2a7994931db3adbc9eaeedb194668ece362
qwe-kev <kevray39@gmail.com>
2023-03-01 20:11:36 +0530

    added mongoose

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const User = require('./models/user');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
+// const User = require('./models/user');
 
 const app = express();
-
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -19,20 +18,24 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('63fe6aa143cf504ad3fe8f5b')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('5baa2528563f16379fc8a610')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
+mongoose.connect('mongodb+srv://kevray:PijtPOeVwvAr46LP@cluster0.lyo81ic.mongodb.net/shop?retryWrites=true')
+.then(() => {
   app.listen(3000);
+})
+.catch(err => {
+  console.log(err);
 });
